e77222efeb18b54f41ce56914735aef47877ed34
paulo <pmanzonemaia@gmail.com>
2020-07-16 17:46:26 -0300

    Dando permissao ao travis

--- src/main/java/com/equitydash/api/config/SwaggerConfig.java
+++ src/main/java/com/equitydash/api/config/SwaggerConfig.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
