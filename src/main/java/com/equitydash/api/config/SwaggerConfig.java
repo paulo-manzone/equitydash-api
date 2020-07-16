c0752fa324c6cdb5fc5ab50d3dc3e48481435189
paulo <pmanzonemaia@gmail.com>
2020-07-16 17:38:08 -0300

    Integrando Travis

--- src/main/java/com/equitydash/api/config/SwaggerConfig.java
+++ src/main/java/com/equitydash/api/config/SwaggerConfig.java
@@ -11,7 +11,6 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
-import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
